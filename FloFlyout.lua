1916392f6408c50c39eb966e527852afa20e3f19
Floraline <floraline@ophiuchus.org>
2012-01-08 20:46:18 +0100

    First step of Flyout configuration UI : Add, remove and change icon of flyouts

--- FloFlyout.lua
+++ FloFlyout.lua
@@ -4,41 +4,42 @@
 
 local VERSION = "4.3.1"
 local NAME = "FloFlyout"
-local SPELLFLYOUT_DEFAULT_SPACING = 4;
-local SPELLFLYOUT_INITIAL_SPACING = 7;
-local SPELLFLYOUT_FINAL_SPACING = 4;
+local SPELLFLYOUT_DEFAULT_SPACING = 4
+local SPELLFLYOUT_INITIAL_SPACING = 7
+local SPELLFLYOUT_FINAL_SPACING = 4
+local STRIPE_COLOR = {r=0.9, g=0.9, b=1}
 
 -------------------------------------------------------------------------------
 -- Variables
 -------------------------------------------------------------------------------
 
 FLOFLYOUT_CONFIG = {
-		flyouts = {
-			--[[ Sample config : each flyout can have a list of spell and an icon
-			[1] = {
-				spells = {
-					[1] = 8024, -- Flametongue
-					[2] = 8033, -- Frostbite
-					[3] = 8232, -- Windfury
-					[4] = 8017, -- RockBite
-					[5] = 51730, -- earthliving
-				},
-				icon = ""
+	flyouts = {
+		--[[ Sample config : each flyout can have a list of spell and an icon
+		[1] = {
+			spells = {
+				[1] = 8024, -- Flametongue
+				[2] = 8033, -- Frostbite
+				[3] = 8232, -- Windfury
+				[4] = 8017, -- RockBite
+				[5] = 51730, -- earthliving
 			},
+			icon = ""
+		},
+		]]
+	},
+	actions = {
+		[1] = {
+			--[[ Sample config : for each talent group there is a list of actions bound to flyouts
+			[13] = 1,
+			[49] = 1,
+			[25] = 1,
 			]]
 		},
-		actions = {
-			[1] = {
-				--[[ Sample config : for each talent group there is a list of actions bound to flyouts
-				[13] = 1,
-				[49] = 1,
-				[25] = 1,
-				]]
-			},
-			[2] = {
-			},
-		}
-	};
+		[2] = {
+		},
+	}
+}
 
 local FloFlyout = {
 	openers = {},
@@ -91,6 +92,18 @@ function FloFlyout_OnLoad(self)
 	SLASH_FLOFLYOUT1 = "/floflyout"
 	SLASH_FLOFLYOUT2 = "/ffo"
 	SlashCmdList["FLOFLYOUT"] = FloFlyout_ReadCmd
+
+	StaticPopupDialogs["CONFIRM_DELETE_FLO_FLYOUT"] = {
+		text = CONFIRM_DELETE_EQUIPMENT_SET,
+		button1 = YES,
+		button2 = NO,
+		OnAccept = function (self) FloFlyout:RemoveFlyout(self.data); FloFlyoutConfigPane_Update(); FloFlyout:ApplyConfig(); end,
+		OnCancel = function (self) end,
+		hideOnEscape = 1,
+		timeout = 0,
+		exclusive = 1,
+		whileDead = 1,
+	}
 
 	self:RegisterEvent("ADDON_LOADED")
 	--self:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
@@ -514,4 +527,353 @@ function FloFlyout:RemoveAction(actionId)
 	if type(actionId) == "string" then actionId = tonumber(actionId) end
 	self.config.actions[GetActiveTalentGroup()][actionId] = nil
 end
+
+function FloFlyoutConfigPane_OnLoad(self)
+	HybridScrollFrame_OnLoad(self)
+	self.update = FloFlyoutConfigPane_Update
+	DEFAULT_CHAT_FRAME:AddMessage( self:GetName().." "..self:GetHeight() )
+	HybridScrollFrame_CreateButtons(self, "FloFlyoutConfigButtonTemplate")
+end
+
+function FloFlyoutConfigPane_OnShow(self)
+	DEFAULT_CHAT_FRAME:AddMessage( self:GetName().." "..self:GetHeight() )
+	HybridScrollFrame_CreateButtons(self, "FloFlyoutConfigButtonTemplate")
+	FloFlyoutConfigPane_Update()
+end
+
+function FloFlyoutConfigPane_OnHide(self)
+	FloFlyoutConfigDialogPopup:Hide()
+end
+
+function FloFlyoutConfigPane_OnUpdate(self)
+	for i = 1, #self.buttons do
+		local button = self.buttons[i]
+		if button:IsMouseOver() then
+			if button.name then
+				button.DeleteButton:Show()
+				button.EditButton:Show()
+			else
+				button.DeleteButton:Hide()
+				button.EditButton:Hide()
+			end
+			button.HighlightBar:Show()
+		else
+			button.DeleteButton:Hide()
+			button.EditButton:Hide()
+			button.HighlightBar:Hide()
+		end
+	end
+end
+
+function FloFlyoutConfigPane_Update()
+	local numRows = #FloFlyout.config.flyouts + 1
+	HybridScrollFrame_Update(FloFlyoutConfigPane, numRows * EQUIPMENTSET_BUTTON_HEIGHT + 20, FloFlyoutConfigPane:GetHeight())
+	
+	local scrollOffset = HybridScrollFrame_GetOffset(FloFlyoutConfigPane)
+	local buttons = FloFlyoutConfigPane.buttons
+	local selectedIdx = FloFlyoutConfigPane.selectedIdx
+	local name, texture, button, flyout
+	for i = 1, #buttons do
+		if i+scrollOffset <= numRows then
+			button = buttons[i]
+			buttons[i]:Show()
+			button:Enable()
+			
+			if i+scrollOffset < numRows then
+				-- Normal flyout button
+				button.name = i+scrollOffset
+				button.text:SetText(button.name);
+				button.text:SetTextColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
+				flyout = FloFlyout.config.flyouts[i+scrollOffset]
+				texture = flyout.icon
+
+				if not texture and flyout.spells[1] then
+					texture = GetSpellTexture(flyout.spells[1])
+				end
+				if texture then
+					button.icon:SetTexture(texture)
+				else
+					button.icon:SetTexture("Interface\\Icons\\INV_Misc_QuestionMark")
+				end
+							
+				if selectedIdx and (i+scrollOffset) == selectedIdx then
+					button.SelectedBar:Show()
+				else
+					button.SelectedBar:Hide()
+				end
+				
+				button.icon:SetSize(36, 36)
+				button.icon:SetPoint("LEFT", 4, 0)
+			else
+				-- This is the Add New button
+				button.name = nil
+				button.text:SetText(PAPERDOLL_NEWEQUIPMENTSET)
+				button.text:SetTextColor(GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b)
+				button.icon:SetTexture("Interface\\PaperDollInfoFrame\\Character-Plus")
+				button.icon:SetSize(30, 30)
+				button.icon:SetPoint("LEFT", 7, 0)
+				button.SelectedBar:Hide()
+			end
+			
+			if (i+scrollOffset) == 1 then
+				buttons[i].BgTop:Show()
+				buttons[i].BgMiddle:SetPoint("TOP", buttons[i].BgTop, "BOTTOM")
+			else
+				buttons[i].BgTop:Hide()
+				buttons[i].BgMiddle:SetPoint("TOP")
+			end
+			
+			if (i+scrollOffset) == numRows then
+				buttons[i].BgBottom:Show()
+				buttons[i].BgMiddle:SetPoint("BOTTOM", buttons[i].BgBottom, "TOP")
+			else
+				buttons[i].BgBottom:Hide()
+				buttons[i].BgMiddle:SetPoint("BOTTOM")
+			end
+			
+			if (i+scrollOffset)%2 == 0 then
+				buttons[i].Stripe:SetTexture(STRIPE_COLOR.r, STRIPE_COLOR.g, STRIPE_COLOR.b)
+				buttons[i].Stripe:SetAlpha(0.1)
+				buttons[i].Stripe:Show()
+			else
+				buttons[i].Stripe:Hide()
+			end
+		else
+			buttons[i]:Hide()
+		end
+	end
+
+end
+
+function FloFlyoutConfigButton_OnClick(self, button, down)
+	if self.name and self.name ~= "" then
+		PlaySound("igMainMenuOptionCheckBoxOn")		-- inappropriately named, but a good sound.
+		FloFlyoutConfigPane.selectedIdx = self.name
+		FloFlyoutConfigPane_Update()
+		FloFlyoutConfigDialogPopup:Hide()
+	else
+		-- This is the "New" button
+		FloFlyoutConfigDialogPopup:Show()
+		FloFlyoutConfigPane.selectedIdx = nil
+		FloFlyoutConfigPane_Update()
+	end
+end
+
+local NUM_FLYOUT_ICONS_SHOWN = 15
+local NUM_FLYOUT_ICONS_PER_ROW = 5
+local NUM_FLYOUT_ICON_ROWS = 3
+local FLYOUT_ICON_ROW_HEIGHT = 36
+local FC_ICON_FILENAMES = {}
+
+function FloFlyoutConfigDialogPopup_OnLoad (self)
+	self.buttons = {}
+	
+	local rows = 0
+	
+	local button = CreateFrame("CheckButton", "FloFlyoutConfigDialogPopupButton1", FloFlyoutConfigDialogPopup, "FloFlyoutConfigPopupButtonTemplate")
+	button:SetPoint("TOPLEFT", 24, -37)
+	button:SetID(1)
+	tinsert(self.buttons, button)
+	
+	local lastPos
+	for i = 2, NUM_FLYOUT_ICONS_SHOWN do
+		button = CreateFrame("CheckButton", "FloFlyoutConfigDialogPopupButton" .. i, FloFlyoutConfigDialogPopup, "FloFlyoutConfigPopupButtonTemplate")
+		button:SetID(i)
+		
+		lastPos = (i - 1) / NUM_FLYOUT_ICONS_PER_ROW
+		if lastPos == math.floor(lastPos) then
+			button:SetPoint("TOPLEFT", self.buttons[i-NUM_FLYOUT_ICONS_PER_ROW], "BOTTOMLEFT", 0, -8)
+		else
+			button:SetPoint("TOPLEFT", self.buttons[i-1], "TOPRIGHT", 10, 0)
+		end
+		tinsert(self.buttons, button)
+	end
+
+	self.SetSelection = function(self, fTexture, Value)
+		if fTexture then
+			self.selectedTexture = Value
+			self.selectedIcon = nil
+		else
+			self.selectedTexture = nil
+			self.selectedIcon = Value
+		end
+	end
+end
+
+function FloFlyoutConfigDialogPopup_OnShow(self)
+	PlaySound("igCharacterInfoOpen")
+	self.name = nil
+	self.isEdit = false
+	RecalculateFloFlyoutConfigDialogPopup()
+end
+
+function FloFlyoutConfigDialogPopup_OnHide(self)
+	FloFlyoutConfigDialogPopup.name = nil
+	FloFlyoutConfigDialogPopup:SetSelection(true, nil)
+	--FloFlyoutConfigDialogPopupEditBox:SetText("")
+	FC_ICON_FILENAMES = nil
+	collectgarbage()
+end
+
+function RecalculateFloFlyoutConfigDialogPopup(iconTexture)
+	local popup = FloFlyoutConfigDialogPopup;
+	
+	if iconTexture then
+		popup:SetSelection(true, iconTexture)
+	else
+		popup:SetSelection(false, 1)
+	end
+	
+	--[[ 
+	Scroll and ensure that any selected equipment shows up in the list.
+	When we first press "save", we want to make sure any selected equipment set shows up in the list, so that
+	the user can just make his changes and press Okay to overwrite.
+	To do this, we need to find the current set (by icon) and move the offset of the FloFlyoutConfigDialogPopup
+	to display it. Issue ID: 171220
+	]]
+	RefreshFlyoutIconInfo()
+	local totalItems = #FC_ICON_FILENAMES
+	local texture, _
+	if popup.selectedTexture then
+		local foundIndex = nil
+		for index=1, totalItems do
+			texture = GetFlyoutIconInfo(index)
+			if string.upper(texture) == popup.selectedTexture then
+				foundIndex = index
+				break
+			end
+		end
+		if foundIndex == nil then
+			foundIndex = 1
+		end
+		-- now make it so we always display at least NUM_FLYOUT_ICON_ROWS of data
+		local offsetnumIcons = floor((totalItems-1)/NUM_FLYOUT_ICONS_PER_ROW)
+		local offset = floor((foundIndex-1) / NUM_FLYOUT_ICONS_PER_ROW)
+		offset = offset + min((NUM_FLYOUT_ICON_ROWS-1), offsetnumIcons-offset) - (NUM_FLYOUT_ICON_ROWS-1)
+		if foundIndex<=NUM_FLYOUT_ICONS_SHOWN then
+			offset = 0			--Equipment all shows at the same place.
+		end
+		FauxScrollFrame_OnVerticalScroll(FloFlyoutConfigDialogPopupScrollFrame, offset*FLYOUT_ICON_ROW_HEIGHT, FLYOUT_ICON_ROW_HEIGHT, nil);
+	else
+		FauxScrollFrame_OnVerticalScroll(FloFlyoutConfigDialogPopupScrollFrame, 0, FLYOUT_ICON_ROW_HEIGHT, nil);
+	end
+	FloFlyoutConfigDialogPopup_Update()
+end
+
+--[[
+RefreshFlyoutIconInfo() counts how many uniquely textured spells the player has in the current flyout. 
+]]
+function RefreshFlyoutIconInfo ()
+	FC_ICON_FILENAMES = {}
+	FC_ICON_FILENAMES[1] = "INV_MISC_QUESTIONMARK"
+	local index = 2
+
+	local popup = FloFlyoutConfigDialogPopup
+	if popup.name then
+		local i
+		local spells = FloFlyout.config.flyouts[popup.name].spells
+		for i = 1, #spells do
+			local itemTexture = GetSpellTexture(spells[i])
+			if itemTexture then
+				FC_ICON_FILENAMES[index] = gsub( strupper(itemTexture), "INTERFACE\\ICONS\\", "" )
+				if FC_ICON_FILENAMES[index] then
+					index = index + 1
+					for j=1, (index-1) do
+						if FC_ICON_FILENAMES[index] == FC_ICON_FILENAMES[j] then
+							FC_ICON_FILENAMES[index] = nil
+							index = index - 1
+							break
+						end
+					end
+				end
+			end
+		end
+	end
+	GetMacroIcons(FC_ICON_FILENAMES)
+	GetMacroItemIcons(FC_ICON_FILENAMES)
+end
+
+function GetFlyoutIconInfo(index)
+	return FC_ICON_FILENAMES[index]
+end
+
+function FloFlyoutConfigDialogPopup_Update()
+	RefreshFlyoutIconInfo()
+
+	local popup = FloFlyoutConfigDialogPopup
+	local buttons = popup.buttons
+	local offset = FauxScrollFrame_GetOffset(FloFlyoutConfigDialogPopupScrollFrame) or 0
+	local button
+	-- Icon list
+	local texture, index, button, realIndex, _
+	for i=1, NUM_FLYOUT_ICONS_SHOWN do
+		local button = buttons[i]
+		index = (offset * NUM_FLYOUT_ICONS_PER_ROW) + i
+		if index <= #FC_ICON_FILENAMES then
+			texture = GetFlyoutIconInfo(index)
+
+			button.icon:SetTexture("INTERFACE\\ICONS\\"..texture)
+			button:Show()
+			if index == popup.selectedIcon then
+				button:SetChecked(1)
+			elseif string.upper(texture) == popup.selectedTexture then
+				button:SetChecked(1)
+				popup:SetSelection(false, index)
+			else
+				button:SetChecked(nil)
+			end
+		else
+			button.icon:SetTexture("")
+			button:Hide()
+		end
+		
+	end
+	
+	-- Scrollbar stuff
+	FauxScrollFrame_Update(FloFlyoutConfigDialogPopupScrollFrame, ceil(#FC_ICON_FILENAMES / NUM_FLYOUT_ICONS_PER_ROW), NUM_FLYOUT_ICON_ROWS, FLYOUT_ICON_ROW_HEIGHT)
+end
+
+function FloFlyoutConfigDialogPopupOkay_Update()
+	local popup = FloFlyoutConfigDialogPopup
+	local button = FloFlyoutConfigDialogPopupOkay
+	
+	if popup.selectedIcon --[[and popup.name]] then
+		button:Enable()
+	else
+		button:Disable()
+	end
+end
+
+function FloFlyoutConfigDialogPopupOkay_OnClick(self, button, pushed)
+	local popup = FloFlyoutConfigDialogPopup
+	local iconTexture
+	if popup.selectedIcon ~= 1 then
+		iconTexture = "INTERFACE\\ICONS\\"..GetFlyoutIconInfo(popup.selectedIcon)
+	end
+
+	if popup.isEdit then
+		-- Modifying a flyout
+		FloFlyout.config.flyouts[popup.name].icon = iconTexture
+	else
+		-- Saving a new flyout
+		FloFlyout.config.flyouts[FloFlyout:AddFlyout()].icon = iconTexture
+	end
+	popup:Hide()
+	FloFlyoutConfigPane_Update()
+	FloFlyout:ApplyConfig()
+end
+
+function FloFlyoutConfigDialogPopupCancel_OnClick ()
+	FloFlyoutConfigDialogPopup:Hide()
+end
+
+function FloFlyoutConfigPopupButton_OnClick(self, button, down)
+	local popup = FloFlyoutConfigDialogPopup
+	local offset = FauxScrollFrame_GetOffset(FloFlyoutConfigDialogPopupScrollFrame) or 0
+	popup.selectedIcon = (offset * NUM_FLYOUT_ICONS_PER_ROW) + self:GetID()
+ 	popup.selectedTexture = nil
+	FloFlyoutConfigDialogPopup_Update()
+	FloFlyoutConfigDialogPopupOkay_Update()
+end
+
 

af3afd370fc4c4052458fc9d1169aad393c7c87e
Floraline <floraline@ophiuchus.org>
2012-01-25 15:43:00 +0100

    Added Licence header (MPLv2) Initialized localization

--- FloFlyout.lua
+++ FloFlyout.lua
@@ -1,3 +1,7 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+
 -------------------------------------------------------------------------------
 -- Constants
 -------------------------------------------------------------------------------
@@ -51,6 +55,8 @@ local FloFlyout = {
 	openers = {},
 	config = FLOFLYOUT_CONFIG
 }
+
+local L = FLOFLYOUT_L10N_STRINGS
 
 -------------------------------------------------------------------------------
 -- Functions

17d70f105f45c366176c4783a128f3584a635d8a
Floraline <floraline@ophiuchus.org>
2013-03-06 10:51:04 +0100

    Bumb version number

--- FloFlyout.lua
+++ FloFlyout.lua
@@ -6,7 +6,7 @@
 -- Constants
 -------------------------------------------------------------------------------
 
-local VERSION = "5.1.4"
+local VERSION = "5.2.4"
 local NAME = "FloFlyout"
 local SPELLFLYOUT_DEFAULT_SPACING = 4
 local SPELLFLYOUT_INITIAL_SPACING = 7

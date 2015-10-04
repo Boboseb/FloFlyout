0e754e8983ebfd8af504319772472d24432d57a6
Floraline <floraline@ophiuchus.org>
2015-10-04 17:07:56 +0200

    Update TOC version

--- FloFlyout.lua
+++ FloFlyout.lua
@@ -6,7 +6,7 @@
 -- Constants
 -------------------------------------------------------------------------------
 
-local VERSION = "6.1.7"
+local VERSION = "6.2.7"
 local NAME = "FloFlyout"
 local SPELLFLYOUT_DEFAULT_SPACING = 4
 local SPELLFLYOUT_INITIAL_SPACING = 7
